7bccd1e95e3a9d2319b6e40f3ebfa418d2145b64
Mohamed-Amiri <mohamedamiri281@gmail.com>
2025-06-11 15:10:12 +0100

    crée la structure de projet

---
+++ src/main/java/org/example/enaaitsupportapp/EnaaITsupportAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.enaaitsupportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaaITsupportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnaaITsupportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/enaaitsupportapp/EnaaITsupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enaaitsupportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnaaITsupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
